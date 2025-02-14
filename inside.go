b455f432fdc21c78052e7e6135bc303d3780fb96
9968187 <9968187@gmail.com>
2025-02-14 12:53:34 +0800

    Modify inside.go

--- inside.go
+++ inside.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Mean stuff option floor hotel campaign.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
